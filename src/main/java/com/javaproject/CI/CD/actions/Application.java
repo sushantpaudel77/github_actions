8b41ba397cb00877ed88cfec3902ecbaf909ab3e
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-05-11 18:34:55 +0545

    code

---
+++ src/main/java/com/javaproject/CI/CD/actions/Application.java
@@ -0,0 +1,19 @@
+package com.javaproject.CI.CD.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Application {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to javaBro";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
