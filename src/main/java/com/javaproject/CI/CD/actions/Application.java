efd16e7628eb2a2688e5fc17fa779c7cf7ee5b3a
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-05-11 20:39:44 +0545

    src added

--- src/main/java/com/javaproject/CI/CD/actions/Application.java
+++ src/main/java/com/javaproject/CI/CD/actions/Application.java
@@ -3,15 +3,16 @@ package com.javaproject.CI.CD.actions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to javaBro";
 	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
